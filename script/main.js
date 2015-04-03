489997368845e216787a8d44f285c44e71697b56
BillyNate <billynate@billynate.com>
2015-04-03 15:40:22 +0200

    Updated the sources shown in the lightbox

--- script/main.js
+++ script/main.js
@@ -2,12 +2,6 @@ $(function() {
 	$(".load-container a").on("click",function() {
 		var hash = $(this).attr("href");
 		var clas = "\\"+hash.replace("#",".") + " ";
-		
-		var re = new RegExp(clas, "g");
-		
-		var css = $(hash).find(".css").val().replace(re,"");
-
-		$(hash).find(".css").val(css);
 		$(".overlay,"+hash).removeClass("hidden");
 		
 		return false;
