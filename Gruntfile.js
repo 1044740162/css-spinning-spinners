71f64e005fee1eb6eae233c9825f75a66a1f6437
BillyNate <billynate@billynate.com>
2015-06-07 09:39:15 +0200

    Moved to grunt-gh-pages instead of a custom script

--- Gruntfile.js
+++ Gruntfile.js
@@ -69,8 +69,46 @@ module.exports = function(grunt) {
     watch: {
       files: ['src/less/*.less','src/jade/*.jade'],
       tasks: ['default']
+    },
+    'gh-pages': {
+      options: {
+        branch: 'gh-pages',
+        only: ['**/*','!README.md','!images/**/*'],
+      },
+      deploy: {
+        options: {
+          move: [{base: 'demo/', src: '**/*', dest: '/'}, {base: 'dist/css/', src: '**/*.css', dest: 'css/'}],
+          replace: [{files: 'index.html', regex: /\.\.\/dist\/css/g, replacement: 'css'}],
+          user: {
+            name: 'Travis CI',
+            email: 'travis@billynate.com'
+          },
+          repo: 'https://' + process.env.GH_TOKEN + '@github.com/' + process.env.TRAVIS_REPO_SLUG,
+          silent: true,
+          message: 'Travis build ' + getDeployMessage()
+        },
+        src: ['demo/**/*','dist/**/*']
+      }
     }
   });
+
+  // get a formatted commit message to review changes from the commit log
+  // github will turn some of these into clickable links
+  function getDeployMessage()
+  {
+    var ret = '\n\n';
+    if(process.env.TRAVIS !== 'true')
+    {
+      ret += 'missing env vars for travis-ci';
+      return ret;
+    }
+    ret += 'branch:       ' + process.env.TRAVIS_BRANCH + '\n';
+    ret += 'SHA:          ' + process.env.TRAVIS_COMMIT + '\n';
+    ret += 'range SHA:    ' + process.env.TRAVIS_COMMIT_RANGE + '\n';
+    ret += 'build id:     ' + process.env.TRAVIS_BUILD_ID  + '\n';
+    ret += 'build number: ' + process.env.TRAVIS_BUILD_NUMBER + '\n';
+    return ret;
+  }
 
   grunt.loadNpmTasks('grunt-contrib-jade');
 
@@ -82,9 +120,11 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-watch');
 
+  grunt.loadNpmTasks('grunt-gh-pages');
+
   grunt.registerTask('default', ['less','autoprefixer','cssmin','jade']);
 
-  grunt.registerTask('deploy', ['default']);
+  grunt.registerTask('deploy', ['gh-pages']);
 
   grunt.registerTask('dev', ['default','watch']);
 
